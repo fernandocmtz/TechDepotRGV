877510e3d25ecdc821c9b7b5f7692fc0ca93d389
SergioL22 <sergioa_la@hotmail.com>
2025-05-04 15:07:32 -0500

    I modifyied order

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,11 +3,26 @@ import { DataTypes } from 'sequelize';
 
 // Define Order model
 export const Order = sequelize.define('Order', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    user_id: { type: DataTypes.INTEGER, allowNull: false },
-    address_id: { type: DataTypes.INTEGER, allowNull: false },
-    status: { type: DataTypes.STRING, allowNull: false }
-}, { tableName: 'orders', timestamps: false });
+    order_id:{
+        type:DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    user_id: {
+        type:DataTypes.INTEGER,
+        allowNull: false
+    },
+    address_id: {
+        type:DataTypes.INTEGER,
+        allowNull: false
+    },
+    status: {
+        type: DataTypes. ENUM('pending', 'shipped', 'delivered', 'cancelled'),
+        allowNull: false
+    }
+}, { tableName: 'orders', 
+    timestamps: false });
+
 
 // Create a new order
 export const createOrder = async (userId, addressId, status) => {
