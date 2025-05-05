e1069208c4b8cf0dd9c47f872418b0a0fd118eb8
SergioL22 <sergioa_la@hotmail.com>
2025-05-05 00:23:09 -0500

    Made the code more oragnized and added some things

--- models/orderModel.js
+++ models/orderModel.js
@@ -3,25 +3,29 @@ import { DataTypes } from 'sequelize';
 
 // Define Order model
 export const Order = sequelize.define('Order', {
-    order_id:{
+    id:{
         type:DataTypes.INTEGER,
-        primaryKey: true,
-        autoIncrement: true
+        autoIncrement: true,
+        primaryKey: true
     },
     user_id: {
         type:DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
+        references:{
+            model:'users',
+            key:'user_id'
+        }
     },
     address_id: {
         type:DataTypes.INTEGER,
-        allowNull: false
+        allowNull: false,
     },
     status: {
-        type: DataTypes. ENUM('pending', 'shipped', 'delivered', 'cancelled'),
+        type: DataTypes.ENUM('pending', 'shipped', 'delivered', 'cancelled'),
         allowNull: false
     }
-}, { tableName: 'orders', 
-    timestamps: false });
+}, { tableName: 'orders',
+     timestamps: true });
 
 
 // Create a new order
