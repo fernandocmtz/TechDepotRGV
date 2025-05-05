311ba901537b9b8343667cb2b885f59713669a0e
jerry <chenyuan.wang@mail.utoronto.ca>
2025-05-05 16:12:21 -0500

    Removed console log

--- config/db.js
+++ config/db.js
@@ -2,10 +2,6 @@ import { Sequelize } from 'sequelize';
 import dotenv from 'dotenv';
 
 dotenv.config(); 
-console.log("🔍 Loaded DB credentials:");
-console.log("DB_USER:", process.env.DB_USER);
-console.log("DB_PASS:", process.env.DB_PASS ? "(set)" : "(not set)");
-console.log("DB_NAME:", process.env.DB_NAME);
 
 const sequelize = new Sequelize(
   process.env.DB_NAME,
