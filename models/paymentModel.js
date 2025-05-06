92bff2437707ea473f8100955beec054ceb518e9
SergioL22 <sergioa_la@hotmail.com>
2025-05-06 15:28:02 -0500

    Code is now organized aas per the PDF file. timestamp now is true.

--- models/paymentModel.js
+++ models/paymentModel.js
@@ -3,12 +3,33 @@ import { DataTypes } from 'sequelize';
 
 // Define Payment model
 export const Payment = sequelize.define('Payment', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    order_id: { type: DataTypes.INTEGER, allowNull: false },
-    amount: { type: DataTypes.FLOAT, allowNull: false },
-    method: { type: DataTypes.STRING, allowNull: false },
-    status: { type: DataTypes.STRING, allowNull: false }
-}, { tableName: 'payments', timestamps: false });
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    order_id:{
+        type:DataTypes.INTEGER,
+        allowNull: false,
+        reference:{
+            model:'orders',
+            key:"order_id"
+        }
+    },
+    status:{
+        type:DataTypes.ENUM('pending', 'completed', 'failed'),
+        allowNull: false
+    },
+    method:{
+        type:DataTypes.ENUM('credit_card', 'paypal', 'crypto', 'cash_on_delivery'),
+        allowNull: false
+    },
+    amount:{
+        type:DataTypes.DECIMAL(10,2),
+        allowNull: false
+    }
+}, { tableName: 'payments',
+     timestamps: true });
 
 // Add a new payment
 export const addPayment = async (orderId, amount, method, status) => {
