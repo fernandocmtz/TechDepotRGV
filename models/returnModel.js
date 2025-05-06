f038c456bb221515640a4c5269694e8cc63913a0
SergioL22 <sergioa_la@hotmail.com>
2025-05-06 15:24:58 -0500

    Formatted the returnModel as per the PDF file from the first part of the project. got rider of order_item_id and made timestamp: true

--- models/returnModel.js
+++ models/returnModel.js
@@ -3,11 +3,25 @@ import { DataTypes } from 'sequelize';
 
 // Define Return model
 export const Return = sequelize.define('Return', {
-    id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true },
-    order_item_id: { type: DataTypes.INTEGER, allowNull: false },
-    reason: { type: DataTypes.STRING, allowNull: false },
-    status: { type: DataTypes.STRING, allowNull: false }
-}, { tableName: 'returns', timestamps: false });
+    id:{
+        type:DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    reason:{
+        type:DataTypes.TEXT,
+        allowNull: false
+    },
+    status:{
+        type:DataTypes.ENUM('requested', 'approved', 'rejected', 'processed'),
+        allowNull:false
+    }
+    
+}, { tableName: 'returns',
+     timestamps: true });
+
+
+
 
 // Process a return
 export const processReturn = async (orderItemId, reason, status) => {
